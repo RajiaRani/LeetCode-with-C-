bc7301d2f35fec5d761a835784e8fe64f7012515
RajiaRani <rajiarani.syal@gmail.com>
2023-12-28 22:08:52 -0600

    uploaded 1st problem

---
+++ 1Twosum.cpp
@@ -0,0 +1,37 @@
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+// You can return the answer in any order.
+
+// Example 1:
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        // step:1 find the array length
+        int n = nums.size();
+        // step2: take the two for loop first is outerloop and second one is innerloop
+        for (int i = 0; i < n - 1; i++)
+        {
+            for (int j = i + 1; j < n; j++)
+            {
+                if (nums[i] + nums[j] == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
